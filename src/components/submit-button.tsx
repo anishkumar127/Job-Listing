6650971be6101daa576b7fb5c02931d95591d13c
“anishkumar127” <anishbishnoi127@gmail.com>
2023-10-23 14:59:47 +0530

    View More Added

--- src/components/submit-button.tsx
+++ src/components/submit-button.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { experimental_useFormStatus as useFormStatus } from "react-dom";
-export default function SubmitButton() {
+export default function SubmitButton({ label }: { label: string }) {
   const { pending } = useFormStatus();
   return (
     <Button disabled={pending} type="submit" variant="outline">
-      Create
+      {label}
     </Button>
   );
 }
